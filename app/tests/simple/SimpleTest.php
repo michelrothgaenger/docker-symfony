1dac704e6458a4c17ae4e92980e8efedeb5af05c
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-03-30 00:43:34 +0100

    add testunit

---
+++ app/tests/simple/SimpleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests\simple;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testFailure()
+    {
+        $this->assertTrue(true);
+    }
+}
