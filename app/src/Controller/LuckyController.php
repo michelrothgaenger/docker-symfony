52ebd69814c9470b35bcf548f2e40d0f0101a14f
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-03-29 20:48:45 +0100

    Add npm nodejs and webpack encore

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -1,19 +1,31 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class LuckyController
+
+class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/",name="home")
      */
     public function number()
     {
         $number = random_int(0, 100);
 
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+    /**
+     * @Route("/todo", name="world")
+     */
+    public function world()
+    {
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Hello World</body></html>'
         );
     }
+
 }
